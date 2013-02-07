924665f88a4f197ceeceb1858a82e15888f49d75
Xiaoyue Cheng <chequered@gmail.com>
2013-02-07 00:34:40 -0600

    adjust the repel and attract forces

--- R/example.R
+++ R/example.R
@@ -6,4 +6,10 @@ state$state=a$names[state$state]
 state=state[-which(is.na(a$x)),]
 state$state=gsub(":.*$","",state$state)
 state$abbr=usCapitals[state$state,2]
-statenbrs=nbrlist(state$abbr,state$x,state$y)
+statenbrs=nbrlist(state$abbr,state$x,state$y)$nbr
+
+statecentroid=nbrlist(state$abbr,state$x,state$y)$centroid
+statecentroid$region=as.character(statecentroid$region)
+rownames(usCapitals)=usCapitals$Abbr
+usCapitals$centroidx=usCapitals$centroidy=NA
+usCapitals[statecentroid$region,c('centroidx','centroidy')]=statecentroid[,2:3]
