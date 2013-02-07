99f4f4e353d4c64fa0f2e44638e32a8749c15a45
Xiaoyue Cheng <chequered@gmail.com>
2013-02-06 22:36:59 -0600

    example of finding neighbors for the map class

---
+++ R/example.R
@@ -0,0 +1,9 @@
+library(maps)
+a=map("state",plot=FALSE,fill=TRUE)
+state=data.frame(x=a$x,y=a$y)
+state$state=cumsum(is.na(a$x))+1
+state$state=a$names[state$state]
+state=state[-which(is.na(a$x)),]
+state$state=gsub(":.*$","",state$state)
+state$abbr=usCapitals[state$state,2]
+statenbrs=nbrlist(state$abbr,state$x,state$y)
