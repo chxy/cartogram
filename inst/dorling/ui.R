4e74c2a30f917c8dc0d75a0a5d9443dee76d88b1
Xiaoyue Cheng <chequered@gmail.com>
2013-02-14 01:00:59 -0600

    create a shiny example for tuning parameters

---
+++ inst/dorling/ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+    
+    headerPanel("Dorling Cartogram"),
+    
+    sidebarPanel(
+        sliderInput("distratio", 
+                    "The attract force happens when the distance is greater than ___ times the sum of radii :", 
+                    min = 1, 
+                    max = 5, 
+                    value = 2,
+                    step = 0.2),
+        
+        sliderInput("iteration", 
+                    "The number of iterations running with one click :", 
+                    min = 10, 
+                    max = 200, 
+                    value = 30,
+                    step = 10),
+        
+        submitButton(text = "Update View")
+    ),
+    
+    mainPanel(
+        
+        plotOutput("distPlot",width="100%", height="525px"),
+        
+        plotOutput("origPlot",width="100%", height="525px")        
+    )
+))
