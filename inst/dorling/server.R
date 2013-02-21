5d736626e1c64b3d55b05a9570035d7c58d38d7f
Xiaoyue Cheng <chequered@gmail.com>
2013-02-21 10:39:26 -0600

    add a shape widget for the shiny gui

--- inst/dorling/server.R
+++ inst/dorling/server.R
@@ -1,8 +1,8 @@
 library(shiny)
 library(maps)
 
-load('../../data/usGeoInfo.rda')
-load('../../data/crimes.rda')
+data(usGeoInfo)
+data(crimes)
 dat=merge(usCapitals,crimes,by.x='Abbr',by.y='abbr')[,c(1,2,4,5,6,9,10,12)]
 dat=dat[-which(dat$Abbr %in% c('AK','HI')),]
 nbrs=statenbrs[names(statenbrs) %in% dat$Abbr]
@@ -17,14 +17,21 @@ shinyServer(function(input, output) {
 
         plot(y~x,state,type='n',xlab="",ylab="",frame=FALSE,xaxt='n',yaxt='n')
         map("state",add=TRUE)
-        points(Latitude~Longitude,dat,type='p',col=2,pch=20)
-        circle(dat$Longitude,dat$Latitude,dat$density,col='pink')
+        points(Latitude~Longitude,dat,type='p',col=2, pch=20)
+        if (input$shape==1){
+            circle(dat$Longitude,dat$Latitude,dat$density,square=TRUE,col='pink')
+        }else{
+            circle(dat$Longitude,dat$Latitude,dat$density,vertex=as.numeric(input$shape),col='pink')
+        }
         text(dat$Longitude,dat$Latitude,dat$Abbr,cex=0.8)
     })
     
     output$distPlot <- reactivePlot(function() {
-               
-        dorling(dat$Abbr,dat$Longitude,dat$Latitude,dat$density,nbrs,dist.ratio=input$distratio, iteration=input$iteration, animation=FALSE)
-                
+        
+        if (input$shape==1) {
+            dorling(dat$Abbr,dat$Longitude,dat$Latitude,dat$density,nbrs,dist.ratio=input$distratio, iteration=input$iteration, animation=FALSE, col='pink', square=TRUE)
+        } else {
+            dorling(dat$Abbr,dat$Longitude,dat$Latitude,dat$density,nbrs,dist.ratio=input$distratio, iteration=input$iteration, polygon.vertex=as.numeric(input$shape), animation=FALSE, col='pink')
+        }             
     })
 })
--- inst/dorling/ui.R
+++ inst/dorling/ui.R
@@ -8,9 +8,9 @@ shinyUI(pageWithSidebar(
         sliderInput("distratio", 
                     "The attract force happens when the distance is greater than ___ times the sum of radii :", 
                     min = 1, 
-                    max = 5, 
-                    value = 2,
-                    step = 0.2),
+                    max = 3, 
+                    value = 1.2,
+                    step = 0.1),
         
         sliderInput("iteration", 
                     "The number of iterations running with one click :", 
@@ -19,12 +19,17 @@ shinyUI(pageWithSidebar(
                     value = 11,
                     step = 10),
         
+        radioButtons("shape",
+                     "The shape of polygons :",
+                     c("Circle"=100, "Triangle"=3, "Square"=1,
+                       "Diamond"=4, "Hexagon"=6, "Octagon"=8)),
+        
         submitButton(text = "Update View")
     ),
     
     mainPanel(
         
-        plotOutput("distPlot",width="100%", height="525px"),
+        plotOutput("distPlot",width="100%", height="430px"),
         
         plotOutput("origPlot",width="100%", height="525px")        
     )
