c7c4575b720eca48d4a58923a3392e61da868258
Xiaoyue Cheng <chequered@gmail.com>
2013-02-21 10:38:53 -0600

    use data(); remove load()

--- inst/example.R
+++ inst/example.R
@@ -1,7 +1,7 @@
 ##### Example 1 ##### crime:population #####
 
-load('../data/usGeoInfo.rda')
-load('../data/crimes.rda')
+data(usGeoInfo)
+data(crimes)
 dat=merge(usCapitals,crimes,by.x='Abbr',by.y='abbr')[,c(1,2,4,5,6,9,10,12)]
 dat=dat[-which(dat$Abbr %in% c('AK','HI')),]
 nbrs=statenbrs[names(statenbrs) %in% dat$Abbr]
@@ -10,11 +10,12 @@ dat$density=sqrt(dat$population/dat$TotalSqMi)
 
 dorling(dat$Abbr,dat$Longitude,dat$Latitude,dat$density,nbrs,iteration=100)
 dorling(dat$Abbr,dat$centroidx,dat$centroidy,dat$density,nbrs,iteration=50,animation=FALSE)
+dorling(dat$Abbr,dat$Longitude,dat$Latitude,dat$density,nbrs,iteration=10,square=TRUE)
 
 ##### Example 2 ##### CHSI:deathpct/ALE #####
 
-load('../data/usGeoInfo.rda')
-load('../data/CHSI_2009.rda')
+data(usGeoInfo)
+data(CHSI_2009)
 library(plyr)
 chsiALE=ddply(chsi,"CHSI_State_Abbr",summarize,ALE=sum(ALE*Population_Size,na.rm=T)/sum(Population_Size),DeathPct=sum(Total_Deaths,na.rm=T)/sum(Population_Size)*100)
 
