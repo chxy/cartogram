1da6149629431430d29d86f22e53d24598ff0b58
Xiaoyue Cheng <chequered@gmail.com>
2013-03-07 16:03:19 -0600

    improve the algorithm

--- inst/ex_gridmap.R
+++ inst/ex_gridmap.R
@@ -1,5 +1,7 @@
 data(usGeoInfo)
-mypal=palette(colors()[c(1:30,71:88)*5+3])
+data(crimes)
+#mypal=palette(colors()[c(1:30,71:88)*5+3])
+palette(sample(c(rainbow(24),colors()[c(1,4:11,13:26,28:29,76:87)*5+3]),48,rep=FALSE))
 
 library(maps)
 library(plyr)
@@ -75,25 +77,43 @@ gridstate$state=gsub(":.+$","",gridstate$polygon)
 gridstate2=gridstate[,c(1,2,5)]
 ydist=length(ygrid1)
 ncell=nrow(gridstate2)
-
 a$crtarea=table(gridstate2$state)[a$state]
+
+plot(y~x,data=gridstate2,pch=15,col=factor(gridstate2$state,levels=a$state))
+text(usCapitals$centroidx,usCapitals$centroidy,usCapitals$Abbr,cex=0.8)
+for (k in 1:150){
 gridstate3=gridstate2
-for (i in 1:ncell){
-    bottom = i-1
-    top    = i+1
-    left   = i-ydist
-    right  = i+ydist
-    if (i %% ydist == 1)  bottom = NA
-    if (i %% ydist == 0)  top    = NA
-    if (i <= ydist)       left   = NA
-    if (i >  ncell-ydist) right  = NA
-    fournbrs=c(bottom,top,left,right)
-    cellnbrs=gridstate3[fournbrs,3]
-    if (any(cellnbrs != gridstate3[i,3], na.rm=TRUE)) {
-        mynbrs=na.omit(cellnbrs[cellnbrs != gridstate3[i,3]])
-        goal=a[mynbrs,'goal']
-        candidate=mynbrs[which.max(goal)]
-        if (a[candidate,'goal']>a[candidate,'crtarea'] && a[gridstate3[i,3],'goal']<a[gridstate3[i,3],'crtarea']) gridstate2[i,3]=candidate
+ord=order(a$goal-a$crtarea)[1:sum((a$goal-a$crtarea)<0)]
+for (j in ord){
+    idxj = which(gridstate2$state==a$state[j])
+    for (i in idxj){
+        bottom = i-1
+        top    = i+1
+        left   = i-ydist
+        right  = i+ydist
+        if (i %% ydist == 1)  bottom = NA
+        if (i %% ydist == 0)  top    = NA
+        if (i <= ydist)       left   = NA
+        if (i >  ncell-ydist) right  = NA
+        fournbrs=c(bottom,top,left,right)
+        cell=gridstate3[i,3]
+        cellnbrs=gridstate3[fournbrs,3]
+        cond1 = all(cellnbrs[1:2]==cell,na.rm=TRUE) & all(cellnbrs[3:4]!=cell,na.rm=TRUE)
+        cond2 = all(cellnbrs[1:2]!=cell,na.rm=TRUE) & all(cellnbrs[3:4]==cell,na.rm=TRUE)
+        if (any(cellnbrs != cell, na.rm=TRUE) && (!cond1) && (!cond2)) {
+            mynbrs=na.omit(cellnbrs[cellnbrs != cell])
+            goal=a[mynbrs,'goal']
+            candidate=mynbrs[which.max(goal)]
+            if ((!candidate %in% a$state[ord[1:j]]) && a[cell,'crtarea']>3) {
+                gridstate2[i,3]=candidate
+                a$crtarea=table(gridstate2$state)[a$state]
+            }
+        }
     }
 }
-plot(y~x,data=gridstate2,pch=15,col=factor(gridstate2$state,levels=a$state))
+cat(k," - ",sum((a$goal-a$crtarea)^2),"\n")
+}
+
+library(randomForest)
+mytree=randomForest(state~x+y+(x+y)+(x-y),gridstate1)
+plot(y~x,data=gridstate1,pch=15,col=predict(mytree,type = "class"))
