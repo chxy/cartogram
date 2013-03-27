40f5e04c6eaae5f100fdebc8983d52a940a7e0c8
Xiaoyue Cheng <chequered@gmail.com>
2013-03-26 23:16:21 -0500

    centroid.polygon cannot be borrowed from package "maps" anymore, so I copied the code to calculate the centroids.
    And fixed a bug.

--- R/centroid_border.R
+++ R/centroid_border.R
@@ -1,3 +1,22 @@
+##' Find the centroid of a polygon.
+##' 
+##' This function is copied from the package \code{maps}.
+##' @param p a data frame of two columns.
+##' @return the x and y coordinates of the centroid.
+##'
+centroid_polygon = function(p) {
+    n = nrow(p)
+    i = c(n, 1:(n - 1))
+    x1 = p[, 1]
+    x2 = p[i, 1]
+    y1 = p[, 2]
+    y2 = p[i, 2]
+    a = x1*y2 - x2*y1
+    s = sum(a)*3
+    if(s == 0) c(mean(x1), mean(y1))
+    else c(sum((x1 + x2)*a)/s, sum((y1 + y2)*a)/s)
+}
+
 ##' Find the centroids for the polygons.
 ##' 
 ##' @param region Region names.
@@ -10,13 +29,12 @@
 ##' state_centroid=centroid(state$abbr,state$x,state$y)
 ##'
 centroid = function(region,x,y){
-    stopifnot(length(x)==length(region) && length(y)==length(region))
+    stopifnot(length(x)==length(region), length(y)==length(region))
     dat=data.frame(r=region,x=x,y=y)
     uniregion=sort(unique(region))
     res=data.frame(region=uniregion,x=NA,y=NA)
     res$region=as.character(res$region)
-    library(maps)
-    res[i,2:3]=centroid.polygon(dat[dat$r==uniregion[i],2:3])
+    for (i in 1:nrow(res)) res[i,2:3]=centroid_polygon(dat[dat$r==uniregion[i],2:3])
     return(res)
 }
 
@@ -32,7 +50,7 @@ centroid = function(region,x,y){
 ##' state_border=border_summary(state$abbr,state$x,state$y)
 ##' 
 border_summary = function(region, x, y){
-    stopifnot(length(x)==length(region) && length(y)==length(region))
+    stopifnot(length(x)==length(region), length(y)==length(region))
     name = sort(unique(region))
     k = length(name)
     digitx = max(min(nchar(as.character(x))),5)
